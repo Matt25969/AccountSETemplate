1961249060fcd7c18aeb95f400a235989ada4e09
VLukovski <valentinlukovski1@gmail.com>
2019-02-11 16:42:24 +0000

    it doesnt work

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,7 +1,51 @@
 package com.qa.persistence.domain;
 
 public class Account {
-	
-	//A comment
 
+	private Long Id;
+	private String firstName;
+	private String lastName;
+	private String accountNumber;
+
+	public Account(Long id, String firstName, String lastName, String accountNumber) {
+		Id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.accountNumber = accountNumber;
+	}
+
+	public Account() {
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
 }
